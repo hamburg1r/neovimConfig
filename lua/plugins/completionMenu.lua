1489982c1360835664f86b7b3b62dfc680691fcf
Hamburgir <shantnuprofid@gmail.com>
2026-02-27 04:04:29 +0530

    feat: Configure blink.cmp keybinds for snippet and single-line scrolling
    - Added <Tab> and <S-Tab> keybinds for snippet_forward and snippet_backward in blink.cmp.
    - Removed snippet_forward and snippet_backward from <C-n> and <C-p> respectively.
    - Modified documentation scroll keybinds (<C-u>, <C-d>) to scroll one line at a time.
    - Added signature scroll keybinds (<C-Up>, <C-Down>) to scroll one line at a time.

--- lua/plugins/completionMenu.lua
+++ lua/plugins/completionMenu.lua
@@ -1,9 +1,20 @@
 return {
 	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "nix shell nixpkgs#gnumake nixpkgs#gcc --command make install_jsregexp"
+	},
+	{
 		'saghen/blink.cmp',
 		event = 'InsertEnter',
 		-- optional: provides snippets for the snippet source
-		dependencies = 'rafamadriz/friendly-snippets',
+		-- dependencies = 'rafamadriz/friendly-snippets',
+		dependencies = {
+			'L3MON4D3/LuaSnip',
+			'Kaiser-Yang/blink-cmp-avante',
+		},
 
 		-- use a release tag to download pre-built binaries
 		version = '*',
@@ -13,8 +24,10 @@ return {
 		build = 'nix run .#build-plugin',
 
 		---@module 'blink.cmp'
-		---@type blink.cmp.Config
 		opts = {
+			snippets = {
+				preset = 'luasnip',
+			},
 			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
 			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
 			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
@@ -31,11 +44,17 @@ return {
 				['<C-c>'] = { 'hide', 'fallback' },
 				['<C-y>'] = { 'select_and_accept' },
 
-				['<C-p>'] = { 'select_prev', 'snippet_backward', 'fallback_to_mappings' },
-				['<C-n>'] = { 'select_next', 'snippet_forward', 'fallback_to_mappings' },
+				['<C-p>'] = { 'select_prev', 'fallback' },
+				['<C-n>'] = { 'select_next', 'fallback' },
 
-				['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-				['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+				['<Tab>'] = { 'snippet_forward', 'fallback' },
+				['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+				['<C-u>'] = { function(cmp) cmp.scroll_documentation_up(1) end, 'fallback' },
+				['<C-d>'] = { function(cmp) cmp.scroll_documentation_down(1) end, 'fallback' },
+
+				['<C-Up>'] = { function(cmp) cmp.scroll_signature_up(1) end, 'fallback' },
+				['<C-Down>'] = { function(cmp) cmp.scroll_signature_down(1) end, 'fallback' },
 
 				['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
 			},
@@ -94,8 +113,26 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { 'snippets', 'lsp', 'path', },
+				default = { 'snippets', 'avante', 'lsp', 'path', 'buffer' },
+				providers = {
+					avante = {
+						module = 'blink-cmp-avante',
+						name = 'Avante',
+						opts = {
+							-- options for blink-cmp-avante
+						}
+					},
+					snippets = {
+						opts = {
+							extended_filetypes = {
+								javascriptreact = { 'html' },
+							}
+						}
+					},
+
+				}
 			},
+
 
 			-- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
 			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
